f0f608314c698b005a3b5fbb5ad88ee974590c4c
safiworks <116169309+safiworks@users.noreply.github.com>
2026-08-11 07:58:05 +0300

    feat(vm): more efficent Stack and Locals handling

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -12,30 +12,129 @@ use janadinite_parse::class::{JVMAccessFlag, JVMCode, OpCode};
 #[derive(Debug, Clone)]
 pub enum VMError {
     StackUnderflow,
+    InvalidLocal(u16),
     InvalidMain(&'static str),
     InvalidConstantPoolEntry(u16),
     NoSuchMethodInClass(String),
     NoSuchClass,
     Corrupted(String),
 }
-#[derive(Debug)]
-struct Stack {
-    data: Vec<u32>,
+
+/// Describes the beginning of a stack frame in a method.
+#[derive(Debug, Clone, Copy, Default)]
+struct StackFrame {
+    #[allow(unused)]
+    meth_ref: u16,
+    stack_start: usize,
+    #[allow(unused)]
+    max_stack: u16,
+    locals_start: usize,
 }
 
-impl Stack {
-    pub fn new(cap: usize) -> Self {
+/// Describes the storage state of the VM thread, such as the stack, heap, and locals.
+#[derive(Debug)]
+struct ThreadContext {
+    stack: Vec<i32>,
+    locals: Vec<i32>,
+    prev_frames: Vec<StackFrame>,
+    current_frame: StackFrame,
+}
+
+impl ThreadContext {
+    /// Creates a new, initial thread context.
+    pub fn new(max_stack: u16, max_locals: u16) -> Self {
         Self {
-            data: Vec::with_capacity(cap),
+            stack: Vec::with_capacity(max_stack as usize),
+            locals: vec![0; max_locals as usize],
+            prev_frames: Vec::new(),
+            current_frame: StackFrame {
+                meth_ref: 0,
+                stack_start: 0,
+                max_stack,
+                locals_start: 0,
+            },
         }
     }
 
-    pub fn ipush(&mut self, v: i32) {
-        self.data.push(v.cast_unsigned());
+    /// Pushes a new stack frame onto the frame stack.
+    pub fn push_frame(
+        &mut self,
+        meth_ref: u16,
+        max_stack: u16,
+        max_locals: u16,
+        args_size: u16,
+    ) -> Result<(), VMError> {
+        let locals_start = self.locals.len();
+        self.locals.resize(locals_start + max_locals as usize, 0);
+        self.stack.reserve(max_stack as usize);
+
+        for i in 0..args_size {
+            self.locals[locals_start + i as usize] =
+                self.stack.pop().ok_or(VMError::StackUnderflow)?;
+        }
+
+        self.prev_frames.push(self.current_frame);
+        self.current_frame = StackFrame {
+            meth_ref,
+            max_stack,
+            locals_start,
+            stack_start: self.stack.len(),
+        };
+
+        Ok(())
     }
 
+    /// Pops the current stack frame from the frame stack.
+    pub fn pop_frame(&mut self) {
+        let popped_frame = self.current_frame;
+        self.current_frame = self.prev_frames.pop().unwrap_or_default();
+
+        self.locals.truncate(popped_frame.locals_start);
+        self.stack.truncate(popped_frame.stack_start);
+    }
+
+    /// Pushes an integer value onto the stack.
+    #[inline(always)]
+    pub fn ipush(&mut self, v: i32) {
+        self.stack.push(v);
+    }
+
+    /// Pops an integer value from the stack.
+    #[inline(always)]
     pub fn ipop(&mut self) -> Option<i32> {
-        self.data.pop().map(|v| v.cast_signed())
+        if self.stack.len() <= self.current_frame.stack_start {
+            return None;
+        }
+        self.stack.pop()
+    }
+
+    /// Loads an integer value from the local variable at the given index.
+    #[inline(always)]
+    pub fn iload(&mut self, index: u16) -> Option<i32> {
+        self.locals
+            .get(index as usize + self.current_frame.locals_start)
+            .copied()
+    }
+
+    /// Stores an integer value into the local variable at the given index.
+    #[must_use = "Returns whether the store was successful"]
+    #[inline(always)]
+    pub fn istore(&mut self, index: u16, value: i32) -> bool {
+        if let Some(local) = self
+            .locals
+            .get_mut(index as usize + self.current_frame.locals_start)
+        {
+            *local = value;
+            true
+        } else {
+            false
+        }
+    }
+
+    #[inline(always)]
+    pub fn iget(&mut self, index: u16) -> Option<&mut i32> {
+        self.locals
+            .get_mut(index as usize + self.current_frame.locals_start)
     }
 }
 
@@ -141,11 +240,9 @@ impl VM {
     fn run_code(
         &self,
         class: &VMClass,
-        mut locals: Box<[i32]>,
+        context: &mut ThreadContext,
         code: &JVMCode,
     ) -> Result<i32, VMError> {
-        let mut stack = Stack::new(code.max_stack() as usize);
-
         let mut last_pc: u16 = 0;
         let mut instr = code.instructions();
 
@@ -154,57 +251,66 @@ impl VM {
                 OpCode::InvokeStatic(ref_idx) => {
                     let (meth_class, method) = self.get_method_or_resolve(class, ref_idx)?;
                     let code = method.code().expect("TODO: methods without code");
-                    let mut locals = vec![0; code.max_locals() as usize].into_boxed_slice();
 
-                    for i in 0..method.args_size() {
-                        let arg = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                        locals[i] = arg;
-                    }
+                    context.push_frame(
+                        ref_idx,
+                        code.max_stack(),
+                        code.max_locals(),
+                        method.args_size() as u16,
+                    )?;
 
-                    let result = self.run_code(meth_class, locals, &code)?;
-                    stack.ipush(result);
+                    let result = self.run_code(meth_class, context, &code)?;
+                    context.pop_frame();
+                    context.ipush(result);
                 }
-                OpCode::Bipush(b) => stack.ipush(b as i32),
+                OpCode::Bipush(b) => context.ipush(b as i32),
                 OpCode::Iload(idx) => {
-                    stack.ipush(locals[idx as usize]);
+                    let v = context.iload(idx as u16).ok_or(VMError::StackUnderflow)?;
+                    context.ipush(v);
                 }
                 OpCode::IStore(idx) => {
-                    locals[idx as usize] = stack.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    if !context.istore(idx as u16, v) {
+                        return Err(VMError::InvalidLocal(idx as u16));
+                    }
                 }
                 OpCode::Iinc(idx, imm) => {
-                    locals[idx as usize] += imm as i32;
+                    context
+                        .iget(idx as u16)
+                        .map(|l| *l += imm as i32)
+                        .ok_or(VMError::InvalidLocal(idx as u16))?;
                 }
                 OpCode::Imul => {
-                    let v2 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    stack.ipush(v1.wrapping_mul(v2));
+                    let v2 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    context.ipush(v1.wrapping_mul(v2));
                 }
                 OpCode::Idiv => {
-                    let v2 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    stack.ipush(v1.wrapping_div(v2));
+                    let v2 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    context.ipush(v1.wrapping_div(v2));
                 }
                 OpCode::Ineg => {
-                    let v = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    stack.ipush(-v);
+                    let v = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    context.ipush(-v);
                 }
                 OpCode::Isub => {
-                    let v2 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v2 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
 
-                    stack.ipush(v1.wrapping_sub(v2));
+                    context.ipush(v1.wrapping_sub(v2));
                 }
                 OpCode::Iadd => {
-                    let v2 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    stack.ipush(v1.wrapping_add(v2));
+                    let v2 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    context.ipush(v1.wrapping_add(v2));
                 }
                 OpCode::Goto(off) => {
                     instr.set_pc(last_pc.saturating_add_signed(off));
                 }
                 OpCode::IfIcmpEq(off) | OpCode::IfIcmpNe(off) => {
-                    let v2 = stack.ipop().ok_or(VMError::StackUnderflow)?;
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v2 = context.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
 
                     match (v1.cmp(&v2), opcode) {
                         (std::cmp::Ordering::Equal, OpCode::IfIcmpEq(_))
@@ -218,7 +324,7 @@ impl VM {
                     }
                 }
                 OpCode::IfNe(off) | OpCode::IfEq(off) => {
-                    let v1 = stack.ipop().ok_or(VMError::StackUnderflow)?;
+                    let v1 = context.ipop().ok_or(VMError::StackUnderflow)?;
                     let v2 = 0;
 
                     match (v1.cmp(&v2), opcode) {
@@ -232,7 +338,7 @@ impl VM {
                         _ => {}
                     }
                 }
-                OpCode::IReturn => return Ok(stack.ipop().ok_or(VMError::StackUnderflow)?),
+                OpCode::IReturn => return Ok(context.ipop().ok_or(VMError::StackUnderflow)?),
                 OpCode::Invalid(i) => {
                     eprintln!("Invalid OpCode: {i}");
                 }
@@ -259,10 +365,7 @@ impl VM {
             return Err(VMError::InvalidMain("no code"));
         };
 
-        self.run_code(
-            &*main_class,
-            vec![0; code.max_locals() as usize].into_boxed_slice(),
-            &code,
-        )
+        let mut context = ThreadContext::new(code.max_stack(), code.max_locals());
+        self.run_code(&*main_class, &mut context, &code)
     }
 }
